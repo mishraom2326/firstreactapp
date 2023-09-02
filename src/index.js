0eded1e2900191a9ff8dd0a5160ad9de1298e6a4
ommishra_uvsoftware <mishraom2323@gmail.com>
2023-09-02 22:58:09 +0530

    commit react app

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
+import './index.css';
+import App from './App';
+import React from 'react';
+
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
